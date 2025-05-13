992b07ad3404ab798b986c28bb99b729b6fc4dfe
xterianhunter <111186435+xterianhunter@users.noreply.github.com>
2025-05-13 05:32:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,5 +112,6 @@ def upload_image():
         flash("Invalid file type. Please upload a .jpg, .jpeg, .png, or .gif file.")
         return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
